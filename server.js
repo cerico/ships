cc56dc21256e9cf1ed7e914366c1e726e6448f44
cerico <garethrobertlee@gmail.com>
2017-12-14 21:26:47 +0000

    change-port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-var port=9002
+var port=59002
 
 app.get('/bundle/bundle.js', function(req,res){
   res.sendFile(path.join(__dirname, 'dist', 'bundle', 'bundle.js'));
