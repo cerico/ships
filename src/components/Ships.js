9e4fa8a68c8fb94e39ebe45bd81ff8492aff0372
cerico <garethrobertlee@gmail.com>
2019-02-03 10:51:09 +0000

    remove unknown code

--- src/components/Ships.js
+++ src/components/Ships.js
@@ -7,25 +7,11 @@ import ShipItem from './ShipItem';
 
 class Ships extends React.Component  {
 
-  processIntegerSequence(input) {
-    console.log(input)
-      var integerSequence = input.split(',')
-        .filter(function(x) {
-            return x.length > 0;
-        })
-        .map(Number);
-    
-      // your code goes here (write your result to the console)
-    }
-    
-
   render (){
     let display
     this.props.location.pathname === '/' ?
       display = 'flex' :
       display = 'none'
-      var input = [1,1,1,2,2,2,1,1,2,2,6,2,1,8]
-      processIntegerSequence(input)
     
     const style = {
       general: {
